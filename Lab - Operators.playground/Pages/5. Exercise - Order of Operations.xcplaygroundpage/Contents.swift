9c77cee8ea019438fb86a19c1d6851bbdad3cae0
alanoud282 <alanod282@gmail.com>
2023-05-08 14:21:02 +0300

    homework

---
+++ Lab - Operators.playground/Pages/5. Exercise - Order of Operations.xcplaygroundpage/Contents.swift
@@ -0,0 +1,31 @@
+/*:
+## Exercise - Order of Operations
+
+ Print out what you think 10 + 2 * 5 evaluates to. Then print out the actual expression (i.e. `print(10 + 2 * 5)`)
+ */
+// What I think it evaluates to: 20
+print(10 + 2 * 5)
+
+// Actual expression:
+print(10 + (2 * 5))
+
+
+//:  In a separate `print` statement, add in the necessary parentheses so that addition takes place before multiplication.
+print((10 + 2) * 5)
+
+
+//:  Print out what you think 4 * 9 - 6 / 2 evaluates to. Then print out the actual expression.
+// What I think it evaluates to: 33
+print(4 * 9 - 6 / 2)
+
+// Actual expression:
+print((4 * 9) - (6 / 2))
+
+
+//:  In a separate `print` statement, add in the necessary parentheses so that the subtraction is prioritized over the multiplication and division.
+print(4 * (9 - 6) / 2)
+
+
+/*:
+[Previous](@previous)  |  page 5 of 8  |  [Next: App Exercise - Complex Fitness Calculations](@next)
+ */
